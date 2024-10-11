737dacff218d12ea540d23927333eff7000e9089
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2024-10-11 22:28:36 +0530

    got to know about how to add documents in database manually...

--- README.md
+++ README.md
@@ -399,7 +399,17 @@ in this I can wrap the route handler in the form of array and yes they will work
             }
       })
 
-
+##
+   - a beautiful and ossam documantation of mongoose
+     https://mongoosejs.com/docs/guide.html
+   - Create a free cluster on MongoDB official website (Mongo Atlas)
+   - Install mongoose library
+   - Connect your application to the Database "Connection-url"/
+   - Call the connectDB function and connect to database before starting application on 7777
+   - Create a userSchema & user Model
+   - Create POST /sigup API to add data to database
+   - Push some documents using API calls from postman
+   - Error Handling using try , catch
 
  
     
--- src/app.js
+++ src/app.js
@@ -1,28 +1,38 @@
 
 const express = require("express");
 
+const connectDB =  require("./config/database");
+
 const app = express();
- 
-app.get("/user",(req,res)=>{
 
+const User = require("./models/user");
+
+app.post("/signup", async (req,res) => {
+         
+        const user = new User ({
+              firstName : "tanvi",
+              lastName  : "sharma",
+              emailId   : "tanvisharma123@gmail.com",
+              password  : "tanvi@123",
+              age       :  23,
+              gender    :  "female"
+        })
       try{
-            throw new Error("Something went wrong bhai!!!...");
-            res.send("ohh No you Found some Errors!!!...");
+            await user.save();
+            res.send("user data has been successfully added...");
+      }catch(err){
+            res.send(400).send("Error found :" + err.message);
       }
-      catch(err){
-            res.status(500).send("Something went wrong Mohit G");
-      }
+     
 })
-
-app.use("/",(err,req,res,next)=>{
-
-      if(err){
-          res.status(500).send("Something went wrong mohit bhai");
-      }
-})
-
-  
-
-app.listen(7777,()=>{
-         console.log("server is running on 7777...")
-})
+ 
+connectDB()
+      .then(() => {
+            console.log("DataBase connection Established Successfully...");
+            app.listen(7777,()=>{
+                  console.log("server is running on 7777...")
+         });
+      })
+      .catch((err) => {
+            console.log("DataBase connection couldn't be Established")
+      })
